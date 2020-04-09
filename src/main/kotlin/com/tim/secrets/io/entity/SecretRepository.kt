ee24f2698906120682730fbb029f7e1ee7d28065
Tim <timothy.d.butterworth@gmail.com>
2020-04-09 09:34:50 -0600

    Find by key using crazy hibernate criteria query

--- src/main/kotlin/com/tim/secrets/io/entity/SecretRepository.kt
+++ src/main/kotlin/com/tim/secrets/io/entity/SecretRepository.kt
@@ -3,8 +3,8 @@ package com.tim.secrets.io.entity
 import arrow.core.Option
 import io.micronaut.configuration.hibernate.jpa.scope.CurrentSession
 import io.micronaut.spring.tx.annotation.Transactional
+import org.hibernate.Session
 import javax.inject.Singleton
-import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull
 
 @Singleton
 open class SecretRepository(
-        @field:PersistenceContext @param:CurrentSession private val entityManager: EntityManager
+        @field:PersistenceContext @param:CurrentSession private val entityManager: Session
 ) {
     @Transactional(readOnly = true)
     open fun findById(id: @NotNull Long?): Option<EncryptedSecret> {
@@ -32,6 +32,15 @@ open class SecretRepository(
 
     @Transactional
     open fun findByKey(key: String): Option<EncryptedSecret> {
-        return Option.fromNullable(entityManager.find(EncryptedSecret::class.java, "key"))
+        val criteriaBuilder = entityManager.criteriaBuilder
+        val query = criteriaBuilder.createQuery(EncryptedSecret::class.java)
+
+        val root = query.from(EncryptedSecret::class.java)
+        query.select(root)
+
+        val params = criteriaBuilder.parameter(String::class.java)
+        query.select(root).where(criteriaBuilder.equal(root.get<String>("key"), params))
+
+        return Option.fromNullable(entityManager.createQuery(query).setParameter(params, key).resultStream.findFirst().orElse(null))
     }
 }
